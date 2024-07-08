bf338630e669b9c55d63104c0ad1f30f1d06d23e
Carlos Dominguez <ixarlie@gmail.com>
2024-07-08 18:39:42 +0200

    symfony workflow utils (#6)

---
+++ src/Symfony/Workflow/TestWorkflow.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Forlond\TestTools\Symfony\Workflow;
+
+use Forlond\TestTools\Symfony\EventDispatcher\TestEventDispatcher;
+use Symfony\Component\Workflow\Definition;
+use Symfony\Component\Workflow\MarkingStore\MarkingStoreInterface;
+use Symfony\Component\Workflow\Workflow;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+final class TestWorkflow extends Workflow
+{
+    private readonly EventDispatcherInterface $dispatcher;
+
+    public function __construct(
+        Definition            $definition,
+        MarkingStoreInterface $markingStore,
+        string                $name = 'unnamed',
+        ?array                $eventsToDispatch = null,
+    ) {
+        $this->dispatcher = new TestEventDispatcher();
+        parent::__construct($definition, $markingStore, $this->dispatcher, $name, $eventsToDispatch);
+    }
+
+    public function getEventDispatcher(): TestEventDispatcher
+    {
+        return $this->dispatcher;
+    }
+}
---
+++ src/Symfony/Workflow/TestWorkflowBuilder.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Forlond\TestTools\Symfony\Workflow;
+
+use Symfony\Component\Workflow\DefinitionBuilder;
+use Symfony\Component\Workflow\MarkingStore\MarkingStoreInterface;
+
+final class TestWorkflowBuilder
+{
+    public string $name = 'unnamed';
+
+    public readonly DefinitionBuilder $definition;
+
+    public ?MarkingStoreInterface $markingStore = null;
+
+    public ?array $eventsToDispatch = null;
+
+    public function __construct()
+    {
+        $this->definition = new DefinitionBuilder();
+    }
+
+    public function build(): TestWorkflow
+    {
+        return new TestWorkflow(
+            $this->definition->build(),
+            $this->markingStore ?? new TestMarkingStorage(),
+            $this->name,
+            $this->eventsToDispatch
+        );
+    }
+}
